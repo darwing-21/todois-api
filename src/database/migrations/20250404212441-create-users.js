849c3800b90f3548cd5f1e2eac839187c553a4de
darwing-21 <darwingarcia2130@gmail.com>
2025-04-04 17:27:39 -0400

    feat(migration): add migration of user and todos

---
+++ src/database/migrations/20250404212441-create-users.js
@@ -0,0 +1,57 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      
+      role: {
+        type: Sequelize.ENUM("admin", "mortal", "cada"),
+        allowNull: false,
+        defaultValue: "mortal",
+      },
+
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("users");
+  },
+};
---
+++ src/database/migrations/20250404212451-create-todos.js
@@ -0,0 +1,52 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("todos", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+      },
+
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      description: {
+        type: Sequelize.TEXT,
+      },
+
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("todos");
+  },
+};
