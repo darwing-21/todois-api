736f4a61f86b31a97a35740943744904620923b2
darwing-21 <darwingarcia2130@gmail.com>
2025-04-04 17:42:36 -0400

    feat(middleware): add auth and role

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../database/models");
+
+const authenticate = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).json({ message: "No token provided" });
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findByPk(decoded.id);
+    if (!user) return res.status(401).json({ message: "Invalid token" });
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
+
+module.exports = authenticate;
